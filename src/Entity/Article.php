4cad0d1926c59bb4268428ad12eb07b0b0b4de34
markoter <markoterdev@outlook.com>
2024-03-05 21:31:45 +0100

    add warehouse and article

---
+++ src/Entity/Article.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $unit_of_measure = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUnitOfMeasure(): ?string
+    {
+        return $this->unit_of_measure;
+    }
+
+    public function setUnitOfMeasure(string $unit_of_measure): static
+    {
+        $this->unit_of_measure = $unit_of_measure;
+
+        return $this;
+    }
+}
