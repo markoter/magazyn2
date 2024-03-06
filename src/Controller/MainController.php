6e47044c5b812a531a85495685b47e1e3b51c33d
markoter <markoterdev@outlook.com>
2024-03-06 17:31:43 +0100

    redirect to login if no user logged in

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 
 class MainController extends AbstractController
 {
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+        // Check if the user is logged in
+        if (!$this->getUser()) {
+            // Redirect to the login page if not logged in
+            return new RedirectResponse($this->router->generate('app_login'));
+        }
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
